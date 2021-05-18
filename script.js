18c092bb0d2affc73b609275d262901fec0911a4
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2021-05-18 18:40:12 +0200

    Initial commit.

---
+++ script.js
@@ -0,0 +1,7 @@
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 34.668181742908736, lng: 135.43048194907362},
+        zoom: 18,
+        mapId: 'a140507ca74f73a8'
+      });
+}
